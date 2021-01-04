27f71bb2e4414ef31582e82c858de17e3d8c8d31
Joshua <153996013@qq.com>
2021-01-03 21:35:10 -0600

    init commit

---
+++ src/main/java/com/JoshuaWeb/ConfigServer/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.JoshuaWeb.ConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
